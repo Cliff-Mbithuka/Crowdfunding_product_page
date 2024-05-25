9777b6c4df5e081e8d392dbf4d32cb4fc456b969
Clifford Musyoka <your_email@example.com>
2024-05-25 23:47:55 +0300

    Open and close modal

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const selectReward = document.querySelectorAll(".select");
 
 //select reward
-selectReward.forEach(function(button){
-    button.addEventListener("click", () => {
-          modal.style.display = "block";
-          document.body.classList.add("fixed");
-          bookmarked.innerHTML = "bookmarked";
-          bookmarked.classList.toggle('changed');
-        });
-  })
+selectReward.forEach(function (button) {
+  button.addEventListener("click", () => {
+    modal.style.display = "block";
+    document.body.classList.add("fixed");
+    bookmarked.innerHTML = "bookmarked";
+    bookmarked.classList.toggle("changed");
+  });
+});
+
+//Opn and close modal
+const modal = document.getElementById("modal");
+const openModal = document.getElementById("openModal");
+const closeIcon = document.getElementsByClassName("close-icon")[0];
+
+openModal.onclick = function () {
+    modal.style.display = "block";
+    document.body.classList.add("fixed");
+  };
+  
+  closeIcon.onclick = function () {
+    modal.style.display = "none";
+    document.body.classList.remove("fixed");
+  };
