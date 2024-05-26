c8083c43dea6f87db2b0551664b684c5362ccb24
Clifford Musyoka <your_email@example.com>
2024-05-27 00:22:18 +0300

    Add local storage

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const selectReward = document.querySelectorAll(".select");
-const submitButtons = document.querySelectorAll('.Continue');
-
-
+const submitButtons = document.querySelectorAll(".Continue");
 
 //select reward
 selectReward.forEach(function (button) {
@@ -43,7 +41,7 @@ radiobtns.forEach(function (radio) {
     pledge.classList.toggle("show-text");
 
     const allPledges = document.querySelectorAll(".pledge");
-    allPledges.forEach(function(pledge){
+    allPledges.forEach(function (pledge) {
       pledge.querySelector(".below").style.display = "none";
     });
 
@@ -53,30 +51,35 @@ radiobtns.forEach(function (radio) {
 
 // *************event listener
 // submit button
-submitButtons.forEach(function(button) {
-  button.addEventListener('click', ClickSubmitButton);
+submitButtons.forEach(function (button) {
+  button.addEventListener("click", ClickSubmitButton);
 });
-
 
 //**** Functions
 // submit button
 function ClickSubmitButton(e) {
-  e.preventDefault(); 
-  const pledgeBox = e.target.closest('.pledge');
-  const dolarInput = pledgeBox.querySelector('.dolar');
+  e.preventDefault();
+  const pledgeBox = e.target.closest(".pledge");
+  const dolarInput = pledgeBox.querySelector(".dolar");
   const value = dolarInput.value;
   const id = new Date().getTime().toString();
-  
+
   //Add to local Storage
   addToLocalStorage(id, value);
-}
 
+  //remove Local storage
+  removeFromLocalStorage(id);
+}
 
 //****************local storage */
-function addToLocalStorage(id, value){
-  
+function addToLocalStorage(id, value) {
+const  plege = { id, value };
+  let items = getLocalStorage();
+  items.push(plege);
+  localStorage.setItem("list", JSON.stringify(items));
+  console.log(items);
 }
 
-
-
-
+function getLocalStorage(){
+return localStorage.getItem("list")?JSON.parse(localStorage.getItem('list')):[];
+}
