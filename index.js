c3d7ad1c15d16e2accc9c1047c0cf49513b2d946
Clifford Musyoka <your_email@example.com>
2024-05-27 11:06:02 +0300

    Add days counter

--- index.js
+++ index.js
@@ -57,29 +57,69 @@ submitButtons.forEach(function (button) {
 
 //**** Functions
 // submit button
+// function ClickSubmitButton(e) {
+//   e.preventDefault();
+//   const pledgeBox = e.target.closest(".pledge");
+//   const dolarInput = pledgeBox.querySelector(".dolar");
+//   const value = dolarInput.value;
+//   const id = new Date().getTime().toString();
+
+//   //Add to local Storage
+//   addToLocalStorage(id, value);
+
+// }
+
+
+
+
+// submit button
 function ClickSubmitButton(e) {
   e.preventDefault();
-  const pledgeBox = e.target.closest(".pledge");
-  const dolarInput = pledgeBox.querySelector(".dolar");
-  const value = dolarInput.value;
+  const pledgeBox = e.target.closest('.pledge');
+  const dolarInput = pledgeBox.querySelector('.dolar');
+  const value = parseFloat(dolarInput.value);
   const id = new Date().getTime().toString();
 
-  //Add to local Storage
-  addToLocalStorage(id, value);
+  // Update total backed amount
+  updateTotalBackedAmount(value);
 
-  //remove Local storage
-  removeFromLocalStorage(id);
+  // Add to local Storage
+  addToLocalStorage(id, value);
 }
+
+
+// Function to update the total backed amount
+function updateTotalBackedAmount(amount) {
+  const totalBackedElement = document.querySelector('div:nth-child(1) h1');
+  let currentAmount = parseFloat(totalBackedElement.textContent.replace(/[^0-9.-]+/g,""));
+  currentAmount += parseFloat(amount);
+  totalBackedElement.textContent = `$${currentAmount.toLocaleString()}`;
+};
 
 //****************local storage */
 function addToLocalStorage(id, value) {
-const  plege = { id, value };
-  let items = getLocalStorage();
-  items.push(plege);
-  localStorage.setItem("list", JSON.stringify(items));
-  console.log(items);
-}
+  const  plege = { id, value };
+    let items = getLocalStorage();
+    items.push(plege);
+    localStorage.setItem("list", JSON.stringify(items));
+  
+  }
+  
+  function getLocalStorage(){
+  return localStorage.getItem("list")?JSON.parse(localStorage.getItem('list')):[];
+  };
 
-function getLocalStorage(){
-return localStorage.getItem("list")?JSON.parse(localStorage.getItem('list')):[];
-}
+
+                   // Days counter
+                   function updateCountdown() {
+                    const today = new Date();
+                    const targetDate = new Date(today.getFullYear(), 9, 17);
+                    const difference = targetDate - today;
+                    const daysLeft = Math.ceil(difference / (1000 * 60 * 60 * 24));
+                    document.getElementById('daysLeft').textContent = daysLeft;
+                  }
+                  updateCountdown();
+                
+                  setInterval(updateCountdown, 1000 * 60 * 60 * 24);
+
+  
